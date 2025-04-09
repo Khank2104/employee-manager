2fe5cd483572c41f29145a2d1f040739578cf01d
Khank2104 <nnkhanh159@gmail.com>
2025-04-09 17:09:51 +0700

    add unit tests for employee manager

---
+++ EmployeeManager.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EmployeeManager.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EmployeeManager.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("602cc297-a0ec-4ed2-b80a-7a14ccb0151b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ EmployeeManager.Tests/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using EmployeeManager.Services;
+using EmployeeManager; // để dùng EmployeeContext
+using EmployeeManager.Data;
+namespace EmployeeManager.Tests
+{
+    [TestClass]
+    public class EmployeeServiceTests
+    {
+        [TestMethod]
+        public void AddEmployee_WithValidData_ShouldReturnSuccess()
+        {
+            // Tạo context giả (In-Memory hoặc context thật tùy theo nhu cầu)
+            var context = new EmployeeContext(); // nếu EmployeeContext chưa có constructor không tham số thì mình sẽ mock
+            var service = new EmployeeService(context);
+
+            // Act
+            bool result = service.AddEmployee("Khánh", "Bảo vệ", "Đang làm", "Bãi A");
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+    }
+}
