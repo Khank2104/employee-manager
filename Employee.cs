8cf8147ea226edbc16166b06566ce0084a9725ce
Khank2104 <nnkhanh159@gmail.com>
2025-04-09 16:49:24 +0700

    add Employee class with properties

---
+++ Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeManager.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int EmployeeId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Role { get; set; }    
+
+        public string Status { get; set; }  
+
+        public string ParkingLotName { get; set; }
+    }
+}
+
+
